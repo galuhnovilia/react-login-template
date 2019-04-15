414f64593ef6533f25551771c0a15f6a2d2e044e
galuh ayu novilia <galuh@softwareseni.com>
2019-04-15 09:33:49 +0700

    update js

--- my-app-project/src/Login.js
+++ my-app-project/src/Login.js
@@ -1,73 +1,86 @@
 import React, { Component } from 'react';
 import './Login.css';
-import {FormErrors} from './FormErrors';
+
 class Login extends Component {
-    constructor(){
-        super();
-        this.state ={
-            email:'',
-            password:'',
-            formErrors: {email: '',password: ''},
-            emailValid: false,
-            passwordValid: false,
-            formValid: false
-        };
-        
-    }
-    handleUserInput(e){
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({[name]:value},
-            ()=>{this.validateField(name, value)});
-    }
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let emailValid = this.state.emailValid;
-        let passwordValid = this.state.passwordValid;
-      
-        switch(fieldName) {
-          case 'email':
-            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-            fieldValidationErrors.email = emailValid ? '' : ' is invalid';
-            break;
-          case 'password':
-            passwordValid = value.length >= 6;
-            fieldValidationErrors.password = passwordValid ? '': ' is too short';
-            break;
-          default:
-            break;
-        }
-        this.setState({formErrors: fieldValidationErrors,
-                        emailValid: emailValid,
-                        passwordValid: passwordValid
-                      }, this.validateForm);
-      }
-      
-      validateForm() {
-        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
-      }
-      errorClass(error) {
-        return(error.length === 0 ? '' : 'has-error');
-     }
+
     render() {
         return (
-        <div id="login">
-            <div className="box-shadow col-md-4 mx-auto">
+        <div id="login">            
+            <div className="box-shadow col-md-4 mx-auto">            
                 <form className="form-login demoForm">
-                    <input type="text" id="fname" className="form-control" placeholder="Enter email" value = {this.state.email} onChange={(event) => this.handleUserInput(event)}/>
-                    <input type="password" id="pass" className="form-control" placeholder="Password" value = {this.state.password} onChange={(event) => this.handleUserInput(event)}/>
-                    <button type="submit" className="btn btn-green w-100" >Login</button>
-                    <h6 className="small">Not registered <a href className="new-account text-green" disabled={!this.state.formValid}>Create an account</a> </h6> 
+                    <h2>Sign up</h2>
+
+                    <LoginForm buttonName="Submit"/>
+                    
                 </form>
             </div>
-            <div className="panel panel-default">
-                <FormErrors formErrors={this.state.formErrors} />
-            </div>
-            <div className={`form-group
-                 ${this.errorClass(this.state.formErrors.email)}`}></div>
+            
         </div>
           
         )
       }
 }
+
+class LoginForm extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      username: "",
+      password: ""
+    }
+    this.clickHandler = this.clickHandler.bind(this)
+    this.setUsername  = this.setUsername.bind(this)
+    this.setPassword     = this.setPassword.bind(this)
+    
+  }
+
+  setUsername(username) {
+    this.setState({username:username})
+  }
+  setPassword(password) {
+    this.setState({password:password})
+  }
+  clickHandler(){
+    alert(`Username : ${this.state.username} Password: ${this.state.password}`)
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="wrap-input">
+          <Input id ="username" labelName="Username " inputType="text" parentFunction={this.setUsername}  />
+        </div>
+        <div className="wrap-input">
+            <Input id ="password" labelName="Password " inputType="password" parentFunction={this.setPassword} /> 
+        </div>
+        <button className="btn btn-green" onClick={this.clickHandler}>{this.props.buttonName}</button>
+      </div>
+    )
+  }
+}
+
+class Input extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputVal: ""
+    }
+    this.changeHandler = this.changeHandler.bind(this)
+  }
+    
+  changeHandler(e) {
+    this.props.parentFunction(e.target.value)
+  }
+  
+  render() {
+    return (
+      <div>
+        {/* <label>{this.props.labelName}</label> */}
+        <input type={this.props.inputType} id={this.props.id} onChange={this.changeHandler} className="form-control input-lg" placeholder={this.props.labelName}/>
+      </div>
+    )
+  }
+}
+
+
 export default  Login;
